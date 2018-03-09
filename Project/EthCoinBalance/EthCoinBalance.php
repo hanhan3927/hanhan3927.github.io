291d2f13ead7ff5a2fc012ab3072a40d9f5aed6b
ChengHan <hanhan3927@gmail.com>
2018-03-09 16:30:45 +0800

    201803091630

--- Project/EthCoinBalance/EthCoinBalance.php
+++ Project/EthCoinBalance/EthCoinBalance.php
@@ -8,13 +8,16 @@
 <body>
 
 <?php
+	echo "Hello world!<br>";
+	echo "asd";
 	header('Content-Type:text/html;charset=utf-8');
 	$url='http://api.f2pool.com/eth/0xcf665c54a11e24dc36c139d3201fd8f684ef0c43';
 	$json=file_get_contents($url);
 	$json_data=json_decode($json,true);
 	
-	$value=json_data[value];
-	echo value;
+	$value=$json_data['value'];
+	
+	echo $value;
 	
 ?>
 
