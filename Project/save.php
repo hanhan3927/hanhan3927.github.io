fb36fe7cb2e5301c7a828db2d8576b098a5e78bd
hanhan3927 <hanahan3927@gmail.com>
2020-05-31 11:19:30 +0800

    delete other project

--- Project/save.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $YourName=$_POST["txtPWD"];
-  $myfile = fopen("result.txt", "w") ;
-  $txt = $YourName;
-  $txt=iconv("UTF-8","big5",$txt);
-  fwrite($myfile, $txt);
-  fclose($myfile);
-?>
-<script type="text/javascript">window.close();</script>
